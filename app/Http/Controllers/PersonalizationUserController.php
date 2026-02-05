b341cf3354ffe4219f993f75d7cc3498dbc2dc6f
mahaandika <agungandikabali@gmail.com>
2026-02-05 21:18:56 +0800

    feat: create personalisation user controller

---
+++ app/Http/Controllers/PersonalizationUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PersonalizationUserController extends Controller
+{
+    public function savePersonalization(Request $request)
+    {
+        $request->validate([
+            'option_ids' => 'required|array',
+            'option_ids.*' => 'exists:personalization_options,id'
+        ]);
+
+        // Menggunakan sync() untuk Composite Pivot Table
+        Auth::user()->personalizations()->sync($request->option_ids);
+
+        return response()->json(['message' => 'Preferences saved successfully']);
+    }
+}
