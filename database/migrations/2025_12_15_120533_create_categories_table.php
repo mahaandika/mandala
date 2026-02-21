554ec9d4a713f88966e2f5c660cb20c50ccd4086
mahaandika <agungandikabali@gmail.com>
2026-02-21 10:59:05 +0800

    fix: add soft delete atribute

--- database/migrations/2025_12_15_120533_create_categories_table.php
+++ database/migrations/2025_12_15_120533_create_categories_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
