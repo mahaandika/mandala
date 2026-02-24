c7eab8d7bf0d75d621843d381917499ace5eec23
mahaandika <agungandikabali@gmail.com>
2026-02-24 12:17:15 +0800

    fix: add fillable status and email verified at

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'phone',
         'address',
         'role',
+        'status',
+        'email_verified_at',
     ];
 
     /**
