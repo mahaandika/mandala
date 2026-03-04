c9b5599b8754a8d13bcaa197e78c5b2b447730ee
mahaandika <agungandikabali@gmail.com>
2026-03-04 18:40:28 +0800

    fix name and description rules

--- app/Http/Requests/Admin/PersonalTypeRequest.php
+++ app/Http/Requests/Admin/PersonalTypeRequest.php
@@ -25,8 +25,8 @@ class PersonalTypeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'max:255'],
-            'label' => ['required', 'max:255'],
+            'name' => ['required', 'max:50'],
+            'description' => ['required', 'max:100'],
             'selection_mode' => ['required', Rule::enum(SelectionMode::class)],
             'selection_type' => ['required', Rule::enum(SelectionType::class)],
             'is_active' => ['required', 'boolean'],
@@ -39,8 +39,8 @@ class PersonalTypeRequest extends FormRequest
         return [
             'name.required' => 'Nama perlu diisi',
             'name.max' => 'Nama tidak boleh lebih dari 255 karakter',
-            'label.required' => 'Label perlu diisi',
-            'label.max' => 'Label tidak boleh lebih dari 255 karakter',
+            'description.required' => 'description perlu diisi',
+            'description.max' => 'description tidak boleh lebih dari 255 karakter',
             'selection_mode.required' => 'Pilihan mode tidak boleh kosong',
             'selection_type.required' => 'Pilihan tipe tidak boleh kosong',
             'selection_type.enum' => 'Pilihan tipe tidak boleh kosong',
