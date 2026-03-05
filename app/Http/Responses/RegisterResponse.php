52d049f341f281924cc6574411981b2a41b191c6
mahaandika <agungandikabali@gmail.com>
2026-03-05 22:40:44 +0800

    refactor regis response

--- app/Http/Responses/RegisterResponse.php
+++ app/Http/Responses/RegisterResponse.php
@@ -6,17 +6,21 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Laravel\Fortify\Contracts\RegisterResponse as ContractsRegisterResponse;
 
-// class RegisterResponse implements ContractsRegisterResponse
-// {
-//     public function toResponse($request)
-//     {
-//         /** @var \App\Models\User $user */
-//         $user = Auth::user();
-//         Auth::logout();
-//         Log::info('singleton jalan');
-//         return redirect()->route('verification.notice.unauthenticated', [
-//             'id' => $user->id,
-//             'hash' => sha1($user->getEmailForVerification()),
-//         ]);
-//     }
-// }
+use function Pest\Laravel\session;
+
+class RegisterResponse implements ContractsRegisterResponse
+{
+    public function toResponse($request)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        Log::info('singleton jalan');
+        return redirect()->route('verification.notice.unauthenticated', [
+            'id' => $user->id,
+            'hash' => sha1($user->getEmailForVerification()),
+        ]);
+    }
+}
