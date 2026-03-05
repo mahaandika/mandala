656e9fe5d7c58c048950a6aed1fe5aa619e83693
mahaandika <agungandikabali@gmail.com>
2026-03-05 23:01:45 +0800

    refactor: comment response

--- app/Http/Responses/RegisterResponse.php
+++ app/Http/Responses/RegisterResponse.php
@@ -8,19 +8,19 @@ use Laravel\Fortify\Contracts\RegisterResponse as ContractsRegisterResponse;
 
 use function Pest\Laravel\session;
 
-class RegisterResponse implements ContractsRegisterResponse
-{
-    public function toResponse($request)
-    {
-        /** @var \App\Models\User $user */
-        $user = Auth::user();
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        Log::info('singleton jalan');
-        return redirect()->route('verification.notice.unauthenticated', [
-            'id' => $user->id,
-            'hash' => sha1($user->getEmailForVerification()),
-        ]);
-    }
-}
+// class RegisterResponse implements ContractsRegisterResponse
+// {
+//     public function toResponse($request)
+//     {
+//         /** @var \App\Models\User $user */
+//         $user = Auth::user();
+//         Auth::logout();
+//         $request->session()->invalidate();
+//         $request->session()->regenerateToken();
+//         Log::info('singleton jalan');
+//         return redirect()->route('verification.notice.unauthenticated', [
+//             'id' => $user->id,
+//             'hash' => sha1($user->getEmailForVerification()),
+//         ]);
+//     }
+// }
