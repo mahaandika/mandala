0a3efbc1491978b9547e85ccf16d76acb6710a51
mahaandika <agungandikabali@gmail.com>
2026-03-11 09:54:09 +0800

    feat: create register controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        Log::info('Proses registrasi manual dimulai', ['email' => $request->email]);
+
+        // 1. Validasi Input Manual
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255', 'min:5'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:100',
+                Rule::unique(User::class),
+            ],
+            'password' => ['required', 'string', 'confirmed', Password::defaults()],
+            'phone' => ['required', 'string', 'max:16', Rule::unique(User::class)],
+            'address' => ['required', 'string', 'max:50'],
+        ]);
+
+        Log::info('Validasi registrasi berhasil dilewati');
+
+        // 2. Buat User Baru
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'phone' => $validated['phone'],
+            'address' => $validated['address'],
+            'role' => Role::CUSTOMER->value, 
+        ]);
+
+        Log::info('User berhasil dibuat ke dalam database', ['user_id' => $user->id]);
+
+
+        // 4. Redirect ke Route Verification Notice
+        return redirect()->route('verification.notice.unauthenticated', [
+            'id' => $user->id,
+            'hash' => sha1($user->getEmailForVerification()),
+        ]);
+    }
+}
