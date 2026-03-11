780f7c57e78e250af1f2c564183cf40f06880418
mahaandika <agungandikabali@gmail.com>
2026-03-11 11:36:47 +0800

    refactor registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -48,18 +48,18 @@ class RegisterController extends Controller
 
             Log::info('User berhasil dibuat di memori sementara', ['user_id' => $user->id]);
 
-            $emailVerif = $user->sendEmailVerificationNotification();
+            // $emailVerif = $user->sendEmailVerificationNotification();
 
-        if (! $emailVerif) {
-            DB::rollBack();
+        // if (! $emailVerif) {
+        //     DB::rollBack();
             
-            Log::error('Gagal mengirim email verifikasi, user batal dibuat', ['user_id' => $user->id]);
+        //     Log::error('Gagal mengirim email verifikasi, user batal dibuat', ['user_id' => $user->id]);
             
-            return back()->withInput()->withErrors([
-                'email' => 'Gagal mengirim email verifikasi. Silakan coba beberapa saat lagi.'
-            ]);
+        //     return back()->withInput()->withErrors([
+        //         'email' => 'Gagal mengirim email verifikasi. Silakan coba beberapa saat lagi.'
+        //     ]);
             
-        } else {
+        // } else {
             DB::commit();
             
             Log::info('Email verifikasi berhasil dikirim', ['user_id' => $user->id]);
@@ -68,7 +68,7 @@ class RegisterController extends Controller
                 'id' => $user->id,
                 'hash' => sha1($user->getEmailForVerification()),
             ]);
-        }
+        // }
 
         } catch (Exception $e) {
             DB::rollBack();
