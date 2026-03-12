1ac4b83cbde755f0e2b1d8878a963e252f1b9d57
mahaandika <agungandikabali@gmail.com>
2026-03-12 13:25:22 +0800

    fix buffer time min duration to 0

--- resources/js/pages/admin/dashboard.tsx
+++ resources/js/pages/admin/dashboard.tsx
@@ -251,8 +251,8 @@ export default function Index() {
             );
 
             const minutesUntilNext = differenceInMinutes(nextResTime, now);
-            const BUFFER = 10;
-            const MIN_DURATION = 20;
+            const BUFFER = 30;
+            const MIN_DURATION = 0;
 
             if (minutesUntilNext - BUFFER >= MIN_DURATION) {
                 const limit = addMinutes(nextResTime, -BUFFER);
@@ -399,8 +399,8 @@ export default function Index() {
                 earliestFutureResTime,
                 now,
             );
-            const BUFFER = 10;
-            const MIN_DURATION = 20;
+            const BUFFER = 30;
+            const MIN_DURATION = 0;
 
             if (minutesUntilNext - BUFFER < MIN_DURATION) {
                 alert(`Waktu mepet! Sisa ${minutesUntilNext} menit.`);
